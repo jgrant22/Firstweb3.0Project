c44a4128d2d3627761663cd67488ebdca2729174
Jared Grant <jgrant@Jareds-MBP.lan>
2022-01-03 22:53:56 -0600

    nav bar and welcom components complete 500 error on next steps debugging tomorrow

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { Navbar, Welcome, Footer, Services, Transactions } from './components';
+const App = () => {
+
+  return (
+    <div className="min-hscreen">
+      <div className="gradient-bg-welcome">
+        <Navbar/>
+        <Welcome/>
+      </div>
+      <Services/>
+      <Transactions/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
